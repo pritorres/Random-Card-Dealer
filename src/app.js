6d9b566632016079fb58947574ab881154a17a3d
priscila torres reyes <priscyla.torres16@hotmail.com>
2021-09-06 14:50:05 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -3,6 +3,30 @@ import "bootstrap";
 import "./style.css";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const iconoSup = document.getElementById("pinta-top");
+  const myNumero = document.getElementById("numero");
+  const iconoInf = document.getElementById("pinta-bottom");
+
+  let numeroPinta = Math.floor(Math.random(0 - 4) * 4);
+  let numeroCarta = Math.floor(Math.random(0 - 13) * 13);
+
+  let myPinta = ["diamond", "heart", "club", "spade"];
+  let myArray = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  myNumero.innerHTML = myArray[numeroCarta];
+  iconoSup.classList.add(myPinta[numeroPinta]);
+  iconoInf.classList.add(myPinta[numeroPinta]);
 };
